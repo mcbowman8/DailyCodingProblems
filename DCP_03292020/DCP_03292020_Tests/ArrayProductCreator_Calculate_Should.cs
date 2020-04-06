2ec78451ea3ad5a229d4259703b74d0aa2953752
Jonathan Hammond <hamman359@gmail.com>
2020-04-05 21:12:46 -0400

    Initial Setup

---
+++ DCP_03292020/DCP_03292020_Tests/ArrayProductCreator_Calculate_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblemTests
+{
+    [TestFixture]
+    public class ArrayProductCreator_Calculate_Should
+    {
+        [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 })]
+        public void ReturnCorrectResult(int[] input, int[] expected)
+        {
+            Assert.False(false);
+        }
+    }
+}
