6fb95f8eb4739319d7b1914ad10f887aa74faa02
Jonathan Hammond <hamman359@gmail.com>
2020-04-05 21:34:58 -0400

    Adding more test cases.

--- DCP_03292020/DCP_03292020_Tests/ArrayProductCreator_Calculate_Should.cs
+++ DCP_03292020/DCP_03292020_Tests/ArrayProductCreator_Calculate_Should.cs
@@ -8,6 +8,10 @@ namespace DailyCodingProblemTests
     public class ArrayProductCreator_Calculate_Should
     {
         [TestCase(new int[] { 1, 1, 1 }, new int[] { 1, 1, 1 })]
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 2, 3, 6 })]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 6, 3, 2 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, new int[] { 120, 60, 40, 30, 24 })]
+        [TestCase(new int[] { 5, 4, 3, 2, 1 }, new int[] { 24, 30, 40, 60, 120 })]
         public void ReturnCorrectResult(int[] input, int[] expected)
         {
             int[] actual = ArrayProductCreator.Calculate(input);
